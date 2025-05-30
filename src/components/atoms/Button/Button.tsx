1eb7a934d1bc18c66aff6a88eb9cc9bc422dbafe
sjoon627 <sjoon627@naver.com>
2025-05-30 19:03:19 +0900

    feat: 버튼 컴포넌트 및 스타일 추가

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from './Button.module.scss';
+
+export type ButtonVariant =
+  | 'account'
+  | 'cartdel'
+  | 'xs'
+  | 'sm'
+  | 'default'
+  | 'rounded'
+  | 'lg'
+  | 'lgWhite'
+  | 'xl';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  color?: 'default' | 'white' | 'none' | 'gray';
+  children: React.ReactNode;
+}
+
+export default function Button({
+  variant = 'default',
+  color = 'default',
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={[styles.button, styles[variant], color !== 'default' && styles[color], className]
+        .filter(Boolean)
+        .join(' ')}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
