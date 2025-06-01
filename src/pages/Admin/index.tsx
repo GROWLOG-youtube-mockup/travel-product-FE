6a48f7bec40e4ee81ee8c520d9b25be3e5ec661f
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-01 12:55:38 +0900

    feat: add Admin page

---
+++ src/pages/Admin/index.tsx
@@ -0,0 +1,10 @@
+const AdminPage = () => {
+  return (
+    <>
+      <h1>Admin Page</h1>
+      <p>This is the admin page where you can manage the application settings.</p>
+    </>
+  );
+};
+
+export default AdminPage;
