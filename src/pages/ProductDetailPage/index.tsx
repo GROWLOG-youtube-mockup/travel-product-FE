80939bc9829c017311bc57d541f89dcc6aa378c1
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 04:41:26 +0900

    feat: 상품 상세 페이지 추가

---
+++ src/pages/ProductDetailPage/index.tsx
@@ -0,0 +1,11 @@
+const ProductDetailPage = () => {
+  return (
+    <>
+      <h1>Product Page</h1>
+      <p>This is the product page where you can find details about our products.</p>
+      <p>Stay tuned for more updates!</p>
+    </>
+  );
+};
+
+export default ProductDetailPage;
