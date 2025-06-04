d80e320cc842d8a0739dddcc0837c01329755774
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 06:36:15 +0900

    feat: router에 페이지 layout 적용

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from '../App';
 import Authorization from '../components/Authorization';
+import AdminLayout from '../layouts/AdminLayout';
+import AuthLayout from '../layouts/AuthLayout';
+import DefaultLayout from '../layouts/DefaultLayout';
+import UserLayout from '../layouts/UserLayout';
 import Admin from '../pages/Admin/index';
 import CartPage from '../pages/Cart/index';
 import ErrorPage from '../pages/Error/index';
@@ -54,53 +58,74 @@ const routes: AppRouteObject[] = [
     element: <App />,
     children: [
       {
-        index: true, // 루트 경로('/')에서 보여질 페이지
-        element: <MainPage /> // 기존 HomePage 대신 MainPage를 홈으로 사용
+        element: <DefaultLayout />, // 일반 사용자용 레이아웃
+        children: [
+          {
+            index: true, // 루트 경로('/')에서 보여질 페이지
+            element: <MainPage /> // 기존 HomePage 대신 MainPage를 홈으로 사용
+          },
+          {
+            path: 'product',
+            element: <ProductPage />
+          },
+          {
+            path: 'product/:id', // 동적 라우팅. 엔드포인트 관련 추가 논의 필요
+            element: <ProductDetailPage />
+          },
+          {
+            path: 'reservation',
+            element: <ReservationPage />
+            // 페이지 레이아웃 구성을 위해 임시로 비활성화
+            // requiredRole: 'USER'
+          },
+          {
+            path: 'cart',
+            element: <CartPage />
+          },
+          {
+            path: 'payment-process',
+            element: <PaymentProcessPage />
+          },
+          {
+            path: 'payment-complete',
+            element: <PaymentCompletePage />
+          }
+        ]
+      },
+
+      {
+        element: <UserLayout />, // 일반 사용자용 레이아웃
+        children: [
+          {
+            path: 'user',
+            element: <UserPage />,
+            requiredRole: 'USER'
+          },
+          {
+            path: 'user-edit',
+            element: <UserEditPage />,
+            requiredRole: 'USER'
+          }
+        ]
       },
       {
-        path: 'admin',
-        element: <Admin />,
-        requiredRole: 'ADMIN'
+        element: <AdminLayout />, // 관리자용 레이아웃
+        children: [
+          {
+            path: 'admin',
+            element: <Admin />,
+            requiredRole: 'ADMIN'
+          }
+        ]
       },
       {
-        path: 'login',
-        element: <LoginPage />
-      },
-      {
-        path: 'product',
-        element: <ProductPage />
-      },
-      {
-        path: 'product/:id', // 동적 라우팅. 엔드포인트 관련 추가 논의 필요
-        element: <ProductDetailPage />
-      },
-      {
-        path: 'reservation',
-        element: <ReservationPage />
-        // 페이지 레이아웃 구성을 위해 임시로 비활성화
-        // requiredRole: 'USER'
-      },
-      {
-        path: 'cart',
-        element: <CartPage />
-      },
-      {
-        path: 'payment-process',
-        element: <PaymentProcessPage />
-      },
-      {
-        path: 'payment-complete',
-        element: <PaymentCompletePage />
-      },
-      {
-        path: 'user',
-        element: <UserPage />,
-        requiredRole: 'USER'
-      },
-      {
-        path: 'user-edit',
-        element: <UserEditPage />,
-        requiredRole: 'USER'
+        element: <AuthLayout />, // 인증 페이지용 레이아웃
+        children: [
+          {
+            path: 'login',
+            element: <LoginPage />
+          }
+        ]
       },
 
       {
