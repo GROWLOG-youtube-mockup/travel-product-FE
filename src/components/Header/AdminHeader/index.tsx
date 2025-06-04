ec8467ef84fd278043f18346546672a34da0723d
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 06:33:18 +0900

    feat: header 컴포넌트 Default/Admin 분리

---
+++ src/components/Header/AdminHeader/index.tsx
@@ -0,0 +1,10 @@
+const AdminHeader = () => {
+  return (
+    <header>
+      <h1>Admin Header</h1>
+      {/* 추후 관리자용 헤더 내용 추가 */}
+    </header>
+  );
+};
+
+export default AdminHeader;
---
+++ src/components/Header/DefaultHeader/index.tsx
@@ -0,0 +1,10 @@
+const DefaultHeader = () => {
+  return (
+    <header>
+      <h1>Default Header</h1>
+      {/* 추후 일반 사용자용 헤더 내용 추가 */}
+    </header>
+  );
+};
+
+export default DefaultHeader;
