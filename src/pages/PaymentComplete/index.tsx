35c1bf44e70d5804f6aa1e309af3968aacb3aa9e
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 14:58:39 +0900

    feat: 결제 완료 페이지 레이아웃 구현

--- src/pages/PaymentComplete/index.tsx
+++ src/pages/PaymentComplete/index.tsx
@@ -1,8 +1,12 @@
+import styles from './PaymentComplete.module.scss';
+
 const PaymentCompletePage = () => {
   return (
     <>
-      <h1>Payment Complete</h1>
-      <p>Thank you for your purchase! Your payment has been successfully processed.</p>
+      <div className={styles.container}>
+        <h1>Payment Complete</h1>
+        <p>Thank you for your purchase! Your payment has been successfully processed.</p>
+      </div>
     </>
   );
 };
