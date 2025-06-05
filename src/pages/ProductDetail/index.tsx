68bd76bebc20089f6579895ecd4c19c0eb55bd37
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 14:57:42 +0900

    feat: 상품 상세 페이지 레이아웃 구현

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,9 +1,13 @@
+import styles from './ProductDetail.module.scss';
+
 const ProductDetailPage = () => {
   return (
     <>
-      <h1>Product Detail Page</h1>
-      <p>Here you can find detailed information about this specific product.</p>
-      <p>Stay tuned for more updates!</p>
+      <div className={styles.container}>
+        <h1>Product Detail Page</h1>
+        <p>Here you can find detailed information about this specific product.</p>
+        <p>Stay tuned for more updates!</p>
+      </div>
     </>
   );
 };
