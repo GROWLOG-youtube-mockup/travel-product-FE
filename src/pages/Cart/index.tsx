13d6575501f7b980be1943430dcfb1bb5a19381c
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 14:56:29 +0900

    feat: 장바구니 페이지 레이아웃 구현

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,8 +1,17 @@
+import styles from './Cart.module.scss';
+
 const CartPage = () => {
   return (
     <>
-      <h1>Shopping Cart</h1>
-      <p>Check your selected items and make adjustments before checkout.</p>
+      <div className={styles.container}>
+        <div className={styles.left__section}>
+          <h1>Shopping Cart</h1>
+          <p>Check your selected items and make adjustments before checkout.</p>
+        </div>
+        <div className={styles.right__section}>
+          <p>modal place</p>
+        </div>
+      </div>
     </>
   );
 };
