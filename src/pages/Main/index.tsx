58f9b5a2a0149e27e8987bbbe0480151760ebed4
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 14:56:49 +0900

    feat: 메인 페이지 레이아웃 구현

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,13 @@
+import styles from './Main.module.scss';
+
 const MainPage = () => {
   return (
     <>
-      <h1>Main Page</h1>
-      <p>Welcome to the main page of our application!</p>
-      <p>This is where you can find the latest updates and features.</p>
+      <div className={styles.container}>
+        <h1>Main Page</h1>
+        <p>Welcome to the main page of our application!</p>
+        <p>This is where you can find the latest updates and features.</p>
+      </div>
     </>
   );
 };
