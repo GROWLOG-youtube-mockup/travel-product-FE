f1e7499717146c82ca186299d48d223babd69338
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-05 14:59:35 +0900

    feat: 회원 정보 수정페이지 레이아웃 구현

--- src/pages/UserEdit/index.tsx
+++ src/pages/UserEdit/index.tsx
@@ -1,8 +1,36 @@
+import styles from './UserEdit.module.scss';
+
 const UserEditPage = () => {
   return (
     <>
-      <h1>User Page</h1>
-      <p>This is the user edit page content.</p>
+      <div className={styles.container}>
+        <h1>User edit Page</h1>
+        <p>This is the user edit page content.</p>
+        <div className={styles.first__section}>
+          <div className={styles.first__section__left}>
+            <h1>계정 정보 변경</h1>
+          </div>
+          <div className={styles.first__section__right}>
+            <h1>입력 구간</h1>
+          </div>
+        </div>
+        <div className={styles.second__section}>
+          <div className={styles.second__section__left}>
+            <h1>비밀번호 재설정</h1>
+          </div>
+          <div className={styles.second__section__right}>
+            <h1>입력 구간</h1>
+          </div>
+        </div>
+        <div className={styles.third__section}>
+          <div className={styles.third__section__left}>
+            <h1>계정 탈퇴</h1>
+          </div>
+          <div className={styles.third__section__right}>
+            <h1>입력 구간</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
