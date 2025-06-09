9421a157468dc13a4fd7bbb0fecf83efabc2f9a8
sjoon627 <sjoon627@naver.com>
2025-06-09 16:35:19 +0900

    feat: defaultheader 컴포넌트 구현 및 스타일 추가

--- src/components/Header/DefaultHeader/index.tsx
+++ src/components/Header/DefaultHeader/index.tsx
@@ -1,8 +1,37 @@
-const DefaultHeader = () => {
+import Label from '../../atoms/Label/Label';
+
+import styles from './DefaultHeader.module.scss';
+
+//props로 받아오도록 되어있는데 추후 zustand에 맞춰서 변경 예정
+interface DefaultHeaderProps {
+  isLoggedIn: boolean;
+  onLogout?: () => void;
+}
+
+const DefaultHeader = ({ isLoggedIn, onLogout }: DefaultHeaderProps) => {
   return (
-    <header>
-      <h1>Default Header</h1>
-      {/* 추후 일반 사용자용 헤더 내용 추가 */}
+    <header className={styles.header}>
+      <div className={styles.logoArea}>
+        <Label className={styles.logo} variant="title" style={{ fontWeight: 700 }}>
+          logo
+        </Label>
+      </div>
+      <nav className={styles.menuArea}>
+        {isLoggedIn ? (
+          <>
+            <Label variant="bodyText">장바구니</Label>
+            <Label variant="bodyText">마이페이지</Label>
+            <span onClick={onLogout} style={{ cursor: 'pointer' }}>
+              <Label variant="bodyText">로그아웃</Label>
+            </span>
+          </>
+        ) : (
+          <>
+            <Label variant="bodyText">회원가입</Label>
+            <Label variant="bodyText">로그인</Label>
+          </>
+        )}
+      </nav>
     </header>
   );
 };
