7178c648aa8aeacd386f8cfd896c1924d48c207f
sjoon627 <sjoon627@naver.com>
2025-06-10 09:47:06 +0900

    feat: 상품 상세 페이지 레이아웃 및 스타일 개선

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -2,13 +2,18 @@ import styles from './ProductDetail.module.scss';
 
 const ProductDetailPage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Product Detail Page</h1>
-        <p>Here you can find detailed information about this specific product.</p>
-        <p>Stay tuned for more updates!</p>
+    <div className={styles.productDetailContainer}>
+      {/* 상단 메인 이미지 */}
+      <div className={styles.mainImage}>image container</div>
+      {/* 하단 2단 그리드 */}
+      <div className={styles.detailGridSimple}>
+        <div className={styles.leftColumnSimple}>
+          <div className={styles.calendarBox}>calender</div>
+          <div className={styles.productInfoBox}>product_info</div>
+        </div>
+        <div className={styles.addCartBox}>add cart</div>
       </div>
-    </>
+    </div>
   );
 };
 
