f0b7e9b5667679717981503f7615c9134e84db5c
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-10 11:21:59 +0900

    style: card slide class 추가

--- src/components/Cards/CardSlide.tsx
+++ src/components/Cards/CardSlide.tsx
@@ -2,9 +2,11 @@ import type { CardProps, CardSlideProps } from '../../type/card';
 
 import Card from './Card';
 
+import './CardSlide.scss';
+
 const CardSlide = ({ cardList }: CardSlideProps) => {
   return (
-    <>
+    <div className="card_slide">
       {cardList.map((card: CardProps) => (
         <Card
           key={card.title}
@@ -14,7 +16,7 @@ const CardSlide = ({ cardList }: CardSlideProps) => {
           price={card.price}
         />
       ))}
-    </>
+    </div>
   );
 };
 
