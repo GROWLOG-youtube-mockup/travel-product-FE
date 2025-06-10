70ecc1afe26fc2df10ccff4cc6bb54b0cb951a72
sjoon627 <sjoon627@naver.com>
2025-06-10 16:48:56 +0900

    fix: div 스타일 및 컴포넌트 수정

--- src/components/Cards/ProductCardLocalRecommend.tsx
+++ src/components/Cards/ProductCardLocalRecommend.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import styles from '../Banner/ProductBanner/ProductBanner.module.scss';
+
 interface LocalRecommendCardProps {
   children: React.ReactNode;
 }
 
 const ProductCardLocalRecommend: React.FC<LocalRecommendCardProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={styles.regionBarItem}>{children}</div>;
 };
 
 export default ProductCardLocalRecommend;
