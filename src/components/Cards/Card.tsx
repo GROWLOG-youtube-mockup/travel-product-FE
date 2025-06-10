d2a29e8b92322efd728d8a8a42d9a0b897720dc7
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-10 11:27:06 +0900

    style: card style 추가

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,5 +1,7 @@
 import type { CardProps } from '../../type/card';
 
+import './Card.scss';
+
 const Card = ({ styleName, image, title, price }: CardProps) => {
   return (
     <div className={`card ${styleName ?? 'normal'}`}>
