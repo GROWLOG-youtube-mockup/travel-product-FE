5fdbfce478ec56cbc5a74c6d582e0041fe52ce72
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-11 10:04:09 +0900

    style: admin layout

--- src/layouts/AdminLayout/index.tsx
+++ src/layouts/AdminLayout/index.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
 import AdminHeader from '../../components/Header/AdminHeader/index';
+import AdminSidebar from '../../components/Sidebar/AdminSidebar/AdminSidebar';
+
+import styles from './AdminLayout.module.scss';
 
 const AdminLayout = () => {
   return (
-    <>
+    <div className={styles['admin-layout']}>
       <AdminHeader />
-      <main>
+      <main className={styles['admin-layout__main']}>
+        <AdminSidebar />
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
