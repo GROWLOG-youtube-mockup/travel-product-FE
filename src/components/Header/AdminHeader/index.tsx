0b8e10f934d23efef97268c8d15b5c9003bb143c
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-11 09:31:17 +0900

    feat: header 내용 추가

--- src/components/Header/AdminHeader/index.tsx
+++ src/components/Header/AdminHeader/index.tsx
@@ -1,8 +1,24 @@
+import styles from './AdminHeader.module.scss';
+
 const AdminHeader = () => {
+  // TODO: 로그인 상태 및 정보에 따라 보여지는 요소가 변경될 수 있도록 수정 필요(store 연동 필요)
+  const userName = '테스트';
+  // const isLoggedIn = sessionStorage.getItem('isLoggedIn') === 'true';
+  const isLoggedIn = true;
+
   return (
-    <header>
-      <h1>Admin Header</h1>
-      {/* 추후 관리자용 헤더 내용 추가 */}
+    <header className={styles['admin-header']}>
+      <h1 className={styles['admin-header__title']}>관리자 대시보드</h1>
+      {isLoggedIn && (
+        <div className={styles['admin-header__user-info']}>
+          <div>
+            <span>안녕하세요 관리자 {userName}님!</span>
+          </div>
+          <button className={styles['admin-header__logout-button']}>
+            <span>로그아웃</span>
+          </button>
+        </div>
+      )}
     </header>
   );
 };
