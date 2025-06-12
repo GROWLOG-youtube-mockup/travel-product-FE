aae335a03ae402692518d46c5990f6f0a3e97e11
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-13 04:13:27 +0900

    style: 장바구니 페이지 모달창 섹션 제거
    - 임시로 클래스명 container로 지정함

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -4,13 +4,8 @@ const CartPage = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.left__section}>
-          <h1>Shopping Cart</h1>
-          <p>Check your selected items and make adjustments before checkout.</p>
-        </div>
-        <div className={styles.right__section}>
-          <p>modal place</p>
-        </div>
+        <h1>Shopping Cart</h1>
+        <p>Check your selected items and make adjustments before checkout.</p>
       </div>
     </>
   );
