578c33a3594392d5e01b273913dbc34382c39747
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-16 16:05:47 +0900

    feat: 로그인, 회원 가입 페이지의 레이아웃 설정

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom';
 
 import DefaultHeader from '../../components/Header/DefaultHeader/DefaultHeader';
 
+import styles from './AuthLayout.module.scss';
+
 const AuthLayout = () => {
   return (
     <>
       <DefaultHeader />
-      <main>
+      <main className={styles.baseForm}>
         <Outlet />
       </main>
     </>
