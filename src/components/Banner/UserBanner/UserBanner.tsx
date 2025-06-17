6d2280bc3c4fd58750b6f8368f1cf7ac822bce0f
sjoon627 <sjoon627@naver.com>
2025-06-17 15:59:49 +0900

    feat: 사용자 배너 및 사용자 페이지 스타일 및 구조 개선

--- src/components/Banner/UserBanner/UserBanner.tsx
+++ src/components/Banner/UserBanner/UserBanner.tsx
@@ -1,8 +1,25 @@
+import styles from './UserBanner.module.scss';
+
 const UserBanner = () => {
   return (
-    <div>
-      <h2>UserBanner</h2>
-      {/* 추후 유저 마이페이지 배너 내용 추가 */}
+    <div className={styles.profileHeader}>
+      <div>
+        <div className={styles.userName}>홍길동</div>
+        <div className={styles.userDesc}>
+          반갑습니다! 여기에 회원님의 정보를 확인하실 수 있습니다.
+        </div>
+      </div>
+      <button className={styles.editBtn}>회원정보 수정</button>
+      <div className={styles.tripSummary}>
+        <div>
+          <div className={styles.tripLabel}>예정된 여행</div>
+          <div className={styles.tripCount}>1</div>
+        </div>
+        <div>
+          <div className={styles.tripLabel}>다녀온 여행</div>
+          <div className={styles.tripCount}>0</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -2,12 +2,34 @@ import styles from './User.module.scss';
 
 const UserPage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>User Page</h1>
-        <p>This is the user page content.</p>
+    <div className={styles.userPageWrapper}>
+      {/* 예정된 여행 리스트 */}
+      <div className={styles.sectionTitle}>예정된 여행</div>
+      <div className={styles.tripCard}>
+        <div className={styles.tripCardHeader}>
+          <span className={styles.orderNumber}>주문번호 #1234</span>
+        </div>
+        <div className={styles.tripInfo}>
+          <div className={styles.tripImage} />
+          <div className={styles.tripDetails}>
+            <div className={styles.tripTitle}>제주도 해변여행</div>
+            <div className={styles.tripDate}>2025년 6월 20일부터</div>
+            <div className={styles.tripDuration}>4일간</div>
+            <div className={styles.tripPeople}>인원 4명</div>
+            <div className={styles.tripPrice}>₩00,000</div>
+          </div>
+        </div>
+        <div className={styles.tripUserInfo}>
+          <div>이름: 000</div>
+          <div>전화번호: 00-0000-0000</div>
+          <div>이메일주소: qwer@qwer.com</div>
+        </div>
+        <div className={styles.tripCardFooter}>
+          <button className={styles.productBtn}>해당 상품 페이지로</button>
+          <button className={styles.cancelBtn}>결제 취소하기</button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
