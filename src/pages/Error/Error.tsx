1bc48df5eff14069d2d950a003e61f70d12f5c2f
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-20 06:31:38 +0900

    feat: errorPage 작성 및 라우팅 설정

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,5 +1,34 @@
-const ErrorPage = () => {
-  return <></>;
-};
+import { useNavigate, useParams } from 'react-router-dom';
 
-export default ErrorPage;
+import Button from '@/components/atoms/Button/Button';
+import DefaultHeader from '@/components/Header/DefaultHeader/DefaultHeader';
+import errorMessages from '@/constants/errorMessages';
+
+import styles from './Error.module.scss';
+
+export default function ErrorPage() {
+  const { status } = useParams<{ status: string }>();
+  const code = status ? Number(status) : 500;
+  const { title, description } = errorMessages[code] ?? errorMessages[500];
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {/* 임시로 로그인 상태의 디폴트 헤더 사용 */}
+      <DefaultHeader isLoggedIn />
+      <div className={styles.container}>
+        <h1 className={styles.code}>{code}</h1>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.desc}>{description}</p>
+        <Button
+          variant="xs"
+          color="default"
+          onClick={() => navigate('/')}
+          className={styles.button}
+        >
+          홈으로 돌아가기
+        </Button>
+      </div>
+    </>
+  );
+}
