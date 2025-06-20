8f0ef7988f9f09eff4591fa13b6be0d03892c00b
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-20 12:41:24 +0900

    feat: 슬라이드 이벤트 핸들 추가

--- src/components/Cards/LocationCardList.tsx
+++ src/components/Cards/LocationCardList.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import type { LocationItem } from '../../type/card';
 
 import LocationCard from './LocationCard';
@@ -10,18 +12,49 @@ type LocationCardListProps = {
 };
 
 const LocationCardList = ({ locationCardList, handleLocationCardClick }: LocationCardListProps) => {
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const handleClickArrow = (dir: string) => {
+    const slider = sliderRef.current;
+
+    if (!slider) return;
+
+    slider.scrollLeft =
+      dir === 'left'
+        ? slider?.scrollLeft - (window.innerWidth - 123)
+        : slider?.scrollLeft + (window.innerWidth - 123);
+  };
+
   return (
-    <div className={styles['cardSlide']}>
-      {locationCardList.map((card) => (
-        <LocationCard
-          key={card.regionId}
-          image={card.image}
-          title={card.title}
-          regionId={card.regionId}
-          handleLocationCardClick={handleLocationCardClick}
-        />
-      ))}
-    </div>
+    <>
+      <button
+        className={styles.arrowButton}
+        onClick={() => {
+          handleClickArrow('left');
+        }}
+      >
+        next
+      </button>
+      <div className={styles['cardSlide']} ref={sliderRef}>
+        {locationCardList.map((card) => (
+          <LocationCard
+            key={card.regionId}
+            image={card.image}
+            title={card.title}
+            regionId={card.regionId}
+            handleLocationCardClick={handleLocationCardClick}
+          />
+        ))}
+      </div>
+      <button
+        className={styles.arrowButton}
+        onClick={() => {
+          handleClickArrow('right');
+        }}
+      >
+        next
+      </button>
+    </>
   );
 };
 
