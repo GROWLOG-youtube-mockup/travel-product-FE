d493304296077796e7a2b1340eb9516976541d21
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-20 10:12:47 +0900

    fix: Main 코드 정리

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -40,6 +40,45 @@ const contentsTitleList: ContentsTitleList[] = [
     styleName: 'normal'
   }
 ];
+const locationList = [
+  {
+    title: '서울',
+    image:
+      'https://cdn.pixabay.com/photo/2016/07/10/05/48/seoul-international-fireworks-festival-1507332_1280.jpg',
+    regionId: 1
+  },
+  {
+    title: '경기・인천',
+    image: 'https://cdn.pixabay.com/photo/2018/05/15/23/02/football-stadium-3404535_1280.jpg',
+    regionId: 2
+  },
+  {
+    title: '강원도',
+    image: 'https://cdn.pixabay.com/photo/2019/07/21/18/30/mountain-4353332_1280.jpg',
+    regionId: 3
+  },
+  {
+    title: '충청도',
+    image: 'https://cdn.pixabay.com/photo/2020/06/17/11/39/taian-5309184_1280.jpg',
+    regionId: 4
+  },
+  {
+    title: '전라도',
+    image: 'https://cdn.pixabay.com/photo/2020/09/02/04/16/image-5537275_1280.jpg',
+    regionId: 5
+  },
+  {
+    title: '경상도',
+    image: 'https://cdn.pixabay.com/photo/2022/08/09/12/10/wolyeong-bridge-7374859_1280.jpg',
+    regionId: 6
+  },
+  {
+    title: '제주도',
+    image: 'https://cdn.pixabay.com/photo/2019/06/11/07/36/shiroyama-hiji-peak-4266254_1280.jpg',
+    regionId: 7
+  }
+];
+const locationTitle = '한국 추천 여행지';
 
 const MainPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -60,46 +99,6 @@ const MainPage = () => {
         console.error('There was a problem with the fetch operation:', error);
       });
   }, []);
-
-  const locationTitle = '한국 추천 여행지';
-  const locationList = [
-    {
-      title: '서울',
-      image:
-        'https://cdn.pixabay.com/photo/2016/07/10/05/48/seoul-international-fireworks-festival-1507332_1280.jpg',
-      regionId: 1
-    },
-    {
-      title: '경기・인천',
-      image: 'https://cdn.pixabay.com/photo/2018/05/15/23/02/football-stadium-3404535_1280.jpg',
-      regionId: 2
-    },
-    {
-      title: '강원도',
-      image: 'https://cdn.pixabay.com/photo/2019/07/21/18/30/mountain-4353332_1280.jpg',
-      regionId: 3
-    },
-    {
-      title: '충청도',
-      image: 'https://cdn.pixabay.com/photo/2020/06/17/11/39/taian-5309184_1280.jpg',
-      regionId: 4
-    },
-    {
-      title: '전라도',
-      image: 'https://cdn.pixabay.com/photo/2020/09/02/04/16/image-5537275_1280.jpg',
-      regionId: 5
-    },
-    {
-      title: '경상도',
-      image: 'https://cdn.pixabay.com/photo/2022/08/09/12/10/wolyeong-bridge-7374859_1280.jpg',
-      regionId: 6
-    },
-    {
-      title: '제주도',
-      image: 'https://cdn.pixabay.com/photo/2019/06/11/07/36/shiroyama-hiji-peak-4266254_1280.jpg',
-      regionId: 7
-    }
-  ];
 
   const handleLocationCardClick = (regionId: number) => {
     navigate({
