87cbcfd6bcce71ed5508a18de74cacdfd3c38a9c
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-20 10:15:34 +0900

    chore: productHandlers 수정

--- src/mocks/handlers/productHandlers.ts
+++ src/mocks/handlers/productHandlers.ts
@@ -3,8 +3,32 @@ import { http, HttpResponse } from 'msw';
 import { products } from '../data/products';
 
 export const productHandlers = [
-  http.get('/products', () => {
-    return HttpResponse.json(products);
+  http.get('/products', ({ request }) => {
+    const url = new URL(request.url);
+    const regionId = url.searchParams.get('regionId');
+    const parentRegionId = url.searchParams.get('parentRegionId');
+    const tagsParam = url.searchParams.get('tags'); // 예: 'Best 추천,예약 폭주'
+
+    let filteredProducts = products;
+
+    if (regionId) {
+      filteredProducts = filteredProducts.filter((p) => p.region.regionId === Number(regionId));
+    }
+
+    if (parentRegionId) {
+      filteredProducts = filteredProducts.filter(
+        (p) => p.region.parentId === Number(parentRegionId)
+      );
+    }
+
+    if (tagsParam) {
+      const tagList = tagsParam.split(',');
+      filteredProducts = filteredProducts.filter((p) =>
+        p.tags.some((tag) => tagList.includes(tag))
+      );
+    }
+
+    return HttpResponse.json(filteredProducts);
   }),
 
   http.get('/products/:product_id', ({ params }) => {
