a6f87211aaa72cf6334d3f90ed5f84e5863612c7
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-23 20:11:02 +0900

    fix: RegionItem type 수정

--- src/type/card.ts
+++ src/type/card.ts
@@ -7,7 +7,7 @@ export type CardProps = {
   handleCardClick: (product_id: number) => void;
 };
 
-export type RegionItem = { title: string; regionId: number; image: string };
+export type RegionItem = { title: string; regionId: number; image: string; description?: string };
 
 export type RegionCardProps = RegionItem & {
   handleRegionCardClick: (item: RegionItem) => void;
