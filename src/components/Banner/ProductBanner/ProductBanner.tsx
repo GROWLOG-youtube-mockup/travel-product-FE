5116dbe1ddbb49c3c79e23fdecc71b88f9ee24b3
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-23 20:11:49 +0900

    style: RegionStore 값 적용

--- src/components/Banner/ProductBanner/ProductBanner.tsx
+++ src/components/Banner/ProductBanner/ProductBanner.tsx
@@ -1,23 +1,22 @@
+import { useRegionStore } from '@/store/RegionStore';
+
 import Label from '../../atoms/Label/Label';
 import ProductCardLocalRecommend from '../../Cards/ProductCardLocalRecommend';
 
 import styles from './ProductBanner.module.scss';
 
 const ProductBanner = () => {
+  const { selectedRegion } = useRegionStore();
+
   return (
     <div className={styles.banner}>
       <div className={styles.wrapper}>
         <div className={styles.textArea}>
-          <Label variant="title" style={{ fontWeight: 700 }}>
-            지역명
-          </Label>
-          <Label variant="bodyText">배너 설명 텍스트입니다.</Label>
+          <Label style={{ fontWeight: 700 }}>{selectedRegion?.title}</Label>
+          <Label>{selectedRegion?.description}</Label>
         </div>
         <div className={styles.imageArea}>
-          <img
-            src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"
-            alt="지역 이미지"
-          />
+          <img src={selectedRegion?.image} alt="지역 이미지" />
         </div>
       </div>
       <div className={styles.regionBarWrapper}>
