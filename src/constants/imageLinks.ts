0e0492570e1ce6078a0a279a0b9b8ed86c4edf92
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-24 09:20:01 +0900

    fix: 이미지 링크 안 나오는 것 확인하여 수정

--- src/constants/imageLinks.ts
+++ src/constants/imageLinks.ts
@@ -1,76 +1,75 @@
 const imageLinks = [
   {
-    name: '서울특별시',
-    image:
-      'https://cdn.pixabay.com/photo/2016/07/10/05/48/seoul-international-fireworks-festival-1507332_1280.jpg'
-  },
-  {
-    name: '경상남도',
-    image: 'https://cdn.pixabay.com/photo/2017/01/14/07/35/sunflower-1978914_960_720.jpg'
-  },
-  {
-    name: '경상북도',
-    image: 'https://cdn.pixabay.com/photo/2021/09/29/06/55/yeongju-6666506_1280.jpg'
-  },
-  {
-    name: '대구광역시',
-    image: 'https://cdn.pixabay.com/photo/2022/02/06/10/39/south-korea-6996746_1280.jpg'
-  },
-  {
-    name: '인천광역시',
-    image: 'https://cdn.pixabay.com/photo/2016/10/20/07/47/songdo-incheon-korea-1754841_1280.jpg'
-  },
-  {
-    name: '전라남도',
-    image: 'https://cdn.pixabay.com/photo/2018/10/21/04/05/night-view-3762230_1280.jpg'
-  },
-  {
-    name: '전라북도',
-    image:
-      'https://sdmntprcentralus.oaiusercontent.com/files/00000000-8300-61f5-9bc2-e828ec8b43b2/raw?se=2025-06-20T03%3A51%3A19Z&sp=r&sv=2024-08-04&sr=b&scid=c0f786f0-dd89-5186-a1ba-abe45058dff7&skoid=c953efd6-2ae8-41b4-a6d6-34b1475ac07c&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-20T01%3A58%3A45Z&ske=2025-06-21T01%3A58%3A45Z&sks=b&skv=2024-08-04&sig=s/K4DKNMQve0UiJ%2BG3Iehl8N7HLsgvOI8vDNSxmuoLY%3D'
-  },
-  {
-    name: '충청남도',
-    image: 'https://cdn.pixabay.com/photo/2020/06/17/11/39/taian-5309184_1280.jpg'
-  },
-  {
-    name: '충청북도',
-    image: 'https://cdn.pixabay.com/photo/2017/06/08/07/08/korea-2382741_1280.jpg'
-  },
-  {
-    name: '강원특별자치도',
+    name: '강원',
     image: 'https://cdn.pixabay.com/photo/2019/07/21/18/30/mountain-4353332_1280.jpg'
   },
   {
-    name: '세종특별자치시',
-    image: 'https://cdn.pixabay.com/photo/2023/06/03/11/26/arboretum-8037495_1280.jpg'
+    name: '경기',
+    image: 'https://cdn.pixabay.com/photo/2019/11/01/05/50/suwon-4593383_1280.jpg'
   },
   {
-    name: '광주광역시',
+    name: '경남',
+    image: 'https://cdn.pixabay.com/photo/2017/01/14/07/35/sunflower-1978914_960_720.jpg'
+  },
+  {
+    name: '경북',
+    image: 'https://cdn.pixabay.com/photo/2021/09/29/06/55/yeongju-6666506_1280.jpg'
+  },
+  {
+    name: '광주',
     image:
       'https://cdn.pixabay.com/photo/2022/10/06/09/29/national-asian-culture-center-7502312_1280.jpg'
   },
   {
-    name: '대전광역시',
+    name: '대구',
+    image: 'https://cdn.pixabay.com/photo/2022/02/06/10/39/south-korea-6996746_1280.jpg'
+  },
+  {
+    name: '대전',
     image:
-      'https://i.namu.wiki/i/eCCEs8QoAalSwbviU5ragOFtuSj13RrtNNNMK_yYxiaizY_crItKnJ98YLcfiv5DajEFq5YExI6NaKoV363negDzqi5Rfh_M_HiKazZxUjWFj3Qy3I6n38ovERZDNx3CO6VTp2tzX6NH6zdGd06L2g.webp'
+      'https://i.namu.wiki/i/W5g8BOSvko5yO_UZ5l0Mnl631f8CUHHBCQ5sIeZ4EuiyjmuUtcGgHCQptne4uG2LZuS-o_VCYOevoqnueum9ftg-ijUloqxtt3Y9vm3vkKCvJ3X26L5qW4w0LO-yx2LpCAYJwMxMpJeoavAASv-j3w.webp'
   },
   {
-    name: '울산광역시',
-    image: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/homigot-4206783_1280.jpg'
-  },
-  {
-    name: '부산광역시',
+    name: '부산',
     image:
       'https://cdn.pixabay.com/photo/2023/01/12/06/16/gamcheon-culture-village-7713364_1280.jpg'
   },
   {
-    name: '제주특별자치도',
+    name: '서울',
+    image:
+      'https://cdn.pixabay.com/photo/2016/07/10/05/48/seoul-international-fireworks-festival-1507332_1280.jpg'
+  },
+  {
+    name: '세종',
+    image: 'https://cdn.pixabay.com/photo/2023/06/03/11/26/arboretum-8037495_1280.jpg'
+  },
+  {
+    name: '울산',
+    image: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/homigot-4206783_1280.jpg'
+  },
+  {
+    name: '인천',
+    image: 'https://cdn.pixabay.com/photo/2016/10/20/07/47/songdo-incheon-korea-1754841_1280.jpg'
+  },
+  {
+    name: '전남',
+    image: 'https://cdn.pixabay.com/photo/2018/10/21/04/05/night-view-3762230_1280.jpg'
+  },
+  {
+    name: '전북',
+    image: 'https://cdn.pixabay.com/photo/2023/02/27/13/57/traditional-village-7818476_1280.jpg'
+  },
+  {
+    name: '제주',
     image: 'https://cdn.pixabay.com/photo/2019/06/11/07/36/shiroyama-hiji-peak-4266254_1280.jpg'
   },
   {
-    name: '경기도',
-    image: 'https://cdn.pixabay.com/photo/2019/11/01/05/50/suwon-4593383_1280.jpg'
+    name: '충남',
+    image: 'https://cdn.pixabay.com/photo/2020/06/17/11/39/taian-5309184_1280.jpg'
+  },
+  {
+    name: '충북',
+    image: 'https://cdn.pixabay.com/photo/2017/06/08/07/08/korea-2382741_1280.jpg'
   }
 ];
 
