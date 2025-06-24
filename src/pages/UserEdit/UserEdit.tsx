ae889fca7fc55c56e85b15c932d875550a8b06af
sjoon627 <sjoon627@naver.com>
2025-06-24 17:51:20 +0900

    feat: 사용자 편집 페이지의 입력 그룹 및 레이아웃 개선

--- src/pages/UserEdit/UserEdit.tsx
+++ src/pages/UserEdit/UserEdit.tsx
@@ -13,30 +13,36 @@ const UserEditPage = () => {
           <p>계정 정보를 변경합니다.</p>
         </div>
         <form className={styles.sectionRight}>
-          <div>
+          <div className={styles.inputGroup}>
             <label>이메일</label>
             <Input type="email" value="qwer@gmail.com" variant="long" disabled />
           </div>
-          <div>
-            <div>
+          <div className={styles.inputRow}>
+            <div className={styles.inputGroup}>
               <label>이름</label>
-              <Input type="text" value="홍길동" variant="short" />
+              <div className={styles.inputInline}>
+                <Input type="text" value="홍길동" variant="short" disabled />
+                <Button variant="account">이름 변경</Button>
+              </div>
             </div>
-            <Button>이름 변경하기</Button>
           </div>
-          <div>
-            <div>
+          <div className={styles.inputRow}>
+            <div className={styles.inputGroup}>
               <label>전화번호</label>
-              <Input type="text" value="010-1234-5678" variant="short" />
+              <div className={styles.inputInline}>
+                <Input type="text" value="010-1234-5678" variant="short" disabled />
+                <Button variant="account">전화번호 변경</Button>
+              </div>
             </div>
-            <Button>전화번호 변경하기</Button>
           </div>
-          <div>
-            <div>
+          <div className={styles.inputRow}>
+            <div className={styles.inputGroup}>
               <label>비밀번호</label>
-              <Input type="password" value="asd****45" variant="short" disabled />
+              <div className={styles.inputInline}>
+                <Input type="password" value="asd****45" variant="short" disabled />
+                <Button variant="account">비밀번호 변경</Button>
+              </div>
             </div>
-            <Button>비밀번호 변경하기</Button>
           </div>
         </form>
       </section>
@@ -48,7 +54,9 @@ const UserEditPage = () => {
           <p>계정 및 개인정보를 삭제합니다.</p>
         </div>
         <div className={styles.sectionRight}>
-          <Button className={styles.danger}>계정 탈퇴하기</Button>
+          <Button className={styles.danger} color="white">
+            계정 탈퇴하기
+          </Button>
         </div>
       </section>
     </div>
