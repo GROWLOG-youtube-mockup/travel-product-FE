cc4d54c844fbeb7144b99785da37bba0dfcd94d5
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-24 15:26:25 +0900

    fix: 반복되는 코드 제거

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -8,19 +8,15 @@ interface ImageGalleryProps {
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
   return (
-    <div className={styles.gallery}>
-      <div className={`${styles.item} ${styles.item1}`}>
-        {images[0] ? <img src={images[0]} alt="sub1" /> : <div className={styles.placeholder} />}
-      </div>
-      <div className={`${styles.item} ${styles.item2}`}>
-        {images[1] ? <img src={images[1]} alt="sub2" /> : <div className={styles.placeholder} />}
-      </div>
-      <div className={`${styles.item} ${styles.item3}`}>
-        {images[2] ? <img src={images[2]} alt="sub3" /> : <div className={styles.placeholder} />}
-      </div>
-      <div className={`${styles.item} ${styles.item4}`}>
-        {images[3] ? <img src={images[3]} alt="sub4" /> : <div className={styles.placeholder} />}
-      </div>
+    <div className={`${styles.gallery} ${images.length === 4 ? styles.four : ''}`}>
+      {images.map((src, idx) => (
+        <div
+          key={idx}
+          className={`${styles.item} ${images.length === 4 ? styles[`item${idx + 1}`] : ''}`}
+        >
+          <img src={src} alt={`image-${idx}`} />
+        </div>
+      ))}
     </div>
   );
 };
