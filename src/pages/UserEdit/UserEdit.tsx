fbe47f082eb8635f701fc984c2c4013477bf951f
sjoon627 <sjoon627@naver.com>
2025-06-24 23:52:26 +0900

    feat: 사용자 편집 페이지 모달이동 추가

--- src/pages/UserEdit/UserEdit.tsx
+++ src/pages/UserEdit/UserEdit.tsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 
 import Button from '../../components/atoms/Button/Button';
 import Input from '../../components/atoms/Input/Input';
+import DeleteAccountForm from '../../components/Modals/DeleteAccountForm';
+import GenericModal from '../../components/Modals/GenericModal';
+import NameChangeForm from '../../components/Modals/NameChangeForm';
+import PasswordChangeForm from '../../components/Modals/PasswordChangeForm';
+import PhoneChangeForm from '../../components/Modals/PhoneChangeForm';
 import type { User } from '../../type/user';
 
 import styles from './UserEdit.module.scss';
@@ -10,6 +15,12 @@ import styles from './UserEdit.module.scss';
 const UserEditPage = () => {
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
+
+  // 모달 상태
+  const [isNameModalOpen, setNameModalOpen] = useState(false);
+  const [isPhoneModalOpen, setPhoneModalOpen] = useState(false);
+  const [isPasswordModalOpen, setPasswordModalOpen] = useState(false);
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -43,7 +54,9 @@ const UserEditPage = () => {
               <label>이름</label>
               <div className={styles.inputInline}>
                 <Input type="text" value={user?.name ?? ''} variant="short" disabled />
-                <Button variant="account">이름 변경</Button>
+                <Button variant="account" onClick={() => setNameModalOpen(true)}>
+                  이름 변경
+                </Button>
               </div>
             </div>
           </div>
@@ -52,7 +65,9 @@ const UserEditPage = () => {
               <label>전화번호</label>
               <div className={styles.inputInline}>
                 <Input type="text" value={user?.phone_number ?? ''} variant="short" disabled />
-                <Button variant="account">전화번호 변경</Button>
+                <Button variant="account" onClick={() => setPhoneModalOpen(true)}>
+                  전화번호 변경
+                </Button>
               </div>
             </div>
           </div>
@@ -61,7 +76,9 @@ const UserEditPage = () => {
               <label>비밀번호</label>
               <div className={styles.inputInline}>
                 <Input type="password" value="" variant="short" disabled />
-                <Button variant="account">비밀번호 변경</Button>
+                <Button variant="account" onClick={() => setPasswordModalOpen(true)}>
+                  비밀번호 변경
+                </Button>
               </div>
             </div>
           </div>
@@ -75,11 +92,41 @@ const UserEditPage = () => {
           <p>계정 및 개인정보를 삭제합니다.</p>
         </div>
         <div className={styles.sectionRight}>
-          <Button className={styles.danger} color="white">
+          <Button className={styles.danger} color="white" onClick={() => setDeleteModalOpen(true)}>
             계정 탈퇴하기
           </Button>
         </div>
       </section>
+
+      {/* 모달들 */}
+      <GenericModal
+        open={isNameModalOpen}
+        onClose={() => setNameModalOpen(false)}
+        title="이름 변경"
+      >
+        <NameChangeForm onSuccess={() => setNameModalOpen(false)} />
+      </GenericModal>
+      <GenericModal
+        open={isPhoneModalOpen}
+        onClose={() => setPhoneModalOpen(false)}
+        title="전화번호 변경"
+      >
+        <PhoneChangeForm onSuccess={() => setPhoneModalOpen(false)} />
+      </GenericModal>
+      <GenericModal
+        open={isPasswordModalOpen}
+        onClose={() => setPasswordModalOpen(false)}
+        title="비밀번호 변경"
+      >
+        <PasswordChangeForm onSuccess={() => setPasswordModalOpen(false)} />
+      </GenericModal>
+      <GenericModal
+        open={isDeleteModalOpen}
+        onClose={() => setDeleteModalOpen(false)}
+        title="계정 탈퇴"
+      >
+        <DeleteAccountForm onSuccess={() => setDeleteModalOpen(false)} />
+      </GenericModal>
     </div>
   );
 };
