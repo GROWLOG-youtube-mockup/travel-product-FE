1dd99653504f2c34879423b043588391d038d2b9
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-26 10:40:22 +0900

    style: 상품 상세 설명 스타일 추가

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,7 +1,37 @@
 import styles from './ProductInfo.module.scss';
 
-const ProductInfo = () => {
-  return <div className={styles.infoWrapper}>ProductInfo</div>;
+interface infoProps {
+  info?: {
+    groupId: number;
+    title: string;
+    type: number;
+    sortOrder: number;
+    items: {
+      itemId: number;
+      content: string;
+      sortOrder: number;
+    }[];
+  }[];
+}
+
+const ProductInfo = ({ info }: infoProps) => {
+  console.log(info);
+  return (
+    <div className={styles.infoWrapper}>
+      {info?.map((group) => (
+        <div key={group.groupId} className={styles.groupWrapper}>
+          <h1 className={styles.title}>{group.title}</h1>
+          <div className={styles.itemWrapper}>
+            {group.items.map((item) => (
+              <div key={item.itemId} className={styles.item}>
+                <p className={styles.content}>{item.content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductInfo;
