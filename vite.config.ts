5ac095e6fdaa43bff650615c3358c1b976c09124
Shin-Yu-1 <shingyu.user@gmail.com>
2025-06-26 21:20:03 +0900

    feat: proxy 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,19 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') }
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.38.61.233:8080',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ''),
+        secure: false // 백엔드가 http이면 안전하게 꺼 둠
+      }
+    }
   }
 });
