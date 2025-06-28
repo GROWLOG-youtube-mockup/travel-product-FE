96fa93a0c1b2a9c85c52e1eb3550f08ca3aa4f5d
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-06-29 02:08:00 +0900

    feat: 자동 에러 페이지 연결 파일 생성 및 api.ts 편집

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,34 +4,29 @@ export const api = axios.create({
   baseURL: '/api',
   withCredentials: true,
   timeout: 10_000,
-
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    'Content-Type': 'application/json'
+    // Authorization 헤더는 아래 인터셉터에서 동적으로 추가
   }
 });
 
-// 에러 페이지 연동
+// 요청 인터셉터에서 accessToken이 있을 때만 Authorization 헤더 추가
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = `Bearer ${token}`;
+  } else if (config.headers && 'Authorization' in config.headers) {
+    delete config.headers['Authorization'];
+  }
+  return config;
+});
+
+//  응답 인터셉터: 상태코드별 에러 페이지 이동
 api.interceptors.response.use(
-  (response) => response,
+  (res) => res,
   (error) => {
     console.error(error);
-    const status = error?.response?.status;
-
-    if (status) {
-      window.location.href = `/error/${status}`;
-    } else {
-      window.location.href = '/error/500';
-    }
-
     return Promise.reject(error);
   }
 );
-
-// api.interceptors.response.use(
-//   (res) => res,
-//   (error) => {
-//     console.error(error);
-//     return Promise.reject(error);
-//   }
-// );
