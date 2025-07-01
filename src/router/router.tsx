bb3814984d029c71fc09365c979aba3e0cd13fe9
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-01 17:14:54 +0900

    fix: 라우터 경로에 id 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -136,13 +136,13 @@ const routes: AppRouteObject[] = [
             // requiredRole: 'USER'
           },
           {
-            path: 'CancelProgress',
+            path: 'CancelProgress/:id',
             element: <CancelProgressPage />
             // 레이아웃 설정을 위한 requiredRole 임시 주석 처리
             // requiredRole: 'USER'
           },
           {
-            path: 'CancelComplete',
+            path: 'CancelComplete/:id',
             element: <CancelCompletePage />
             // 레이아웃 설정을 위한 requiredRole 임시 주석 처리
             // requiredRole: 'USER'
