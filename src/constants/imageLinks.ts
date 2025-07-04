5ca75b3f903155717325776cd4bc4b08ecc5d867
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-04 10:02:39 +0900

    fix: 대전 이미지 안 나오는 것 확인하여 링크 수정

--- src/constants/imageLinks.ts
+++ src/constants/imageLinks.ts
@@ -27,7 +27,7 @@ const imageLinks = [
   {
     name: '대전',
     image:
-      'https://i.namu.wiki/i/W5g8BOSvko5yO_UZ5l0Mnl631f8CUHHBCQ5sIeZ4EuiyjmuUtcGgHCQptne4uG2LZuS-o_VCYOevoqnueum9ftg-ijUloqxtt3Y9vm3vkKCvJ3X26L5qW4w0LO-yx2LpCAYJwMxMpJeoavAASv-j3w.webp'
+      'https://images.unsplash.com/photo-1700929259310-10a0ba52de68?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
     name: '부산',
